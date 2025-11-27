4bfc48cba4df5b94a3ab7ce6150191b1315f5de2
Henrique <hebaja@yahoo.com.br>
2025-11-27 00:05:45 -0300

    ex02 done(need refactoring) & editing .gitignore

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,45 @@
+#pragma  once
+
+#include <ostream>
+class Fixed
+{
+	private:
+		int	fixedPoint;
+		static const int fractBits;
+
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(const int n);
+		Fixed(const float n);
+		Fixed(const Fixed &other);
+		void operator = (const Fixed &other);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		int		operator>(const Fixed &other) const;
+		int		operator<(const Fixed &other) const;
+		int		operator>=(const Fixed &other) const;
+		int		operator<=(const Fixed &other) const;
+		int		operator==(const Fixed &other) const;
+		int		operator!=(const Fixed &other) const;
+		// Implement arithmetic operators
+		Fixed	operator+(const Fixed &other) const;
+		Fixed	operator-(const Fixed &other) const;	
+		Fixed	operator*(const Fixed &other) const;	
+		Fixed	operator/(const Fixed &other) const;	
+	
+		Fixed&	operator++();
+		Fixed	operator++(int);
+		Fixed&	operator--();
+		Fixed	operator--(int);
+
+		static	Fixed&	min(Fixed &a, Fixed &b);
+		static	const Fixed&	min(const Fixed &a, const Fixed &b);
+		static	Fixed&	max(Fixed &a, Fixed &b);
+		static	const Fixed&	max(const Fixed &a, const Fixed &b);
+};
+
+std::ostream&	operator<<(std::ostream& out, const Fixed& fixed);
---
+++ ex02/main.cpp
@@ -0,0 +1,100 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	{
+		Fixed	a(10);
+		Fixed	b(10);
+		Fixed	c(7);
+		Fixed	d(42);
+
+		Fixed	e(10.1f);
+		Fixed	f(10.1f);
+		Fixed	g(1.125f);
+		Fixed	h(42.42f);
+
+		std::cout << "Operator == (int) must be 1 -> " << (a == b) << std::endl;
+		std::cout << "Operator == (int) must be 0 -> " << (a == c) << std::endl;
+		std::cout << "Operator == (flo) must be 1 -> " << (e == f) << std::endl;
+		std::cout << "Operator == (flo) must be 0 -> " << (e == h) << std::endl;
+		
+		std::cout << "Operator > (int)  must be 1 -> " << (a > c) << std::endl;
+		std::cout << "Operator > (int)  must be 0 -> " << (a > b) << std::endl;
+		std::cout << "Operator > (int)  must be 0 -> " << (a > d) << std::endl;
+		std::cout << "Operator > (flo)  must be 1 -> " << (e > g) << std::endl;
+		std::cout << "Operator > (flo)  must be 0 -> " << (e > f) << std::endl;
+		std::cout << "Operator > (flo)  must be 0 -> " << (a > h) << std::endl;
+		
+		std::cout << "Operator < (int)  must be 1 -> " << (c < a) << std::endl;
+		std::cout << "Operator < (int)  must be 0 -> " << (b < a) << std::endl;
+		std::cout << "Operator < (int)  must be 0 -> " << (d < a) << std::endl;
+		std::cout << "Operator < (flo)  must be 1 -> " << (g < e) << std::endl;
+		std::cout << "Operator < (flo)  must be 0 -> " << (f < e) << std::endl;
+		std::cout << "Operator < (flo)  must be 0 -> " << (h < e) << std::endl;
+		
+		std::cout << "Operator >= (int) must be 1 -> " << (a >= c) << std::endl;
+		std::cout << "Operator >= (int) must be 1 -> " << (a >= b) << std::endl;
+		std::cout << "Operator >= (int) must be 0 -> " << (a >= d) << std::endl;
+		std::cout << "Operator >= (flo) must be 1 -> " << (e >= g) << std::endl;
+		std::cout << "Operator >= (flo) must be 1 -> " << (e >= f) << std::endl;
+		std::cout << "Operator >= (flo) must be 0 -> " << (a >= h) << std::endl;
+		
+		std::cout << "Operator <= (int) must be 1 -> " << (c <= a) << std::endl;
+		std::cout << "Operator <= (int) must be 1 -> " << (b <= a) << std::endl;
+		std::cout << "Operator <= (int) must be 0 -> " << (d <= a) << std::endl;
+		std::cout << "Operator <= (flo) must be 1 -> " << (g <= e) << std::endl;
+		std::cout << "Operator <= (flo) must be 1 -> " << (f <= e) << std::endl;
+		std::cout << "Operator <= (flo) must be 0 -> " << (h <= e) << std::endl;
+		
+		std::cout << "Operator != (int) must be 1 -> " << (a != c) << std::endl;
+		std::cout << "Operator != (int) must be 0 -> " << (a != b) << std::endl;
+		std::cout << "Operator != (flo) must be 1 -> " << (e != g) << std::endl;
+		std::cout << "Operator != (flo) must be 0 -> " << (e != f) << std::endl;
+	}
+	{
+		Fixed	a(1.0f);
+		Fixed	b(2.0f);
+
+		Fixed	c = a + b;
+		std::cout << "Operator + (int) must be 3 -> " << c << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << b++ << " " << b << std::endl;
+		std::cout << --a << std::endl;
+		std::cout << b-- << " " << b << std::endl;
+
+	}
+	{
+		Fixed	const a(42);
+		Fixed	const b(24);
+		Fixed	c = a + b;
+		std::cout << "a + b == " << c << std::endl;
+		c = a - b;
+		std::cout << "a - b == " << c << std::endl;
+		c = a * b;
+		std::cout << "a * b == " << c << std::endl;
+		c = a / b;
+		std::cout << "a / b == " << c << std::endl;
+	}
+	{
+		Fixed	a(1.3f);
+		Fixed	b(1.2f);
+		Fixed	res;
+
+		res = Fixed::min(a, b);
+		std::cout << res << std::endl;
+		res = Fixed::max(a, b);
+		std::cout << res << std::endl;
+	}
+	{
+		Fixed	const a(1.3f);
+		Fixed	const b(1.2f);
+		Fixed	res;
+
+		res = Fixed::min(a, b);
+		std::cout << res << std::endl;
+		res = Fixed::max(a, b);
+		std::cout << res << std::endl;
+	}
+	return (0);
+}
